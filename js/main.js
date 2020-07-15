836bfd50d7e2f9ae9548885d1a5d2cb3bda275f4
shah1dss <ckeiter@bk.ru>
2020-07-15 21:34:32 +0300

    Сверстан хедер и настроены слайдеры

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 $(function(){
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="arrows-right">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="arrows-left">',
+        asNavFor: '.slider-dots'
+    });
 
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
+    });
 
-
-  
 });
