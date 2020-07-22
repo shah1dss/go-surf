26d6bc98c47dd6ec0029c0d79a1ab7c5f7be6cde
shah1dss <ckeiter@bk.ru>
2020-07-22 18:55:11 +0300

    Верстка полностью готова

--- js/main.js
+++ js/main.js
@@ -35,6 +35,13 @@ $(function(){
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="arrows-left">',
     });
 
+    $('.shop__slider').slick({
+      infinite: true,
+      fade: true,
+      prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="arrows-right">',
+      nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="arrows-left">',
+  });
+
     $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="images/plus.svg" alt="plus"></li></div><div class="quantity-button quantity-down"><img src="images/minus.svg" alt="minus"></li></div></div>').insertAfter('.quantity input');
     $('.quantity').each(function() {
       var spinner = $(this),
@@ -67,11 +74,22 @@ $(function(){
       });
 
     });
-        $('.summ').html('$350');
-    $('.quantity-button').on('click', function(){
-        let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() -1) * $('.summ').data('guests') ;
-        $('.summ').html('$' + summ);
-    });
 
-    
+    $('.summ').html('$350');
+    $('.quantity-button').on('click', function(){
+        var parents = $(this).parents('.holder-slider__info');
+        let summ = $('.summ', parents).data('nights') * $('.nights', parents).val() + $('.summ', parents).data('guests') * $('.guests', parents).val();
+        $('.summ', parents).html('$' + summ);
+        });
+
+
+        $('.quantity').each(function() {
+        var parents = $(this).parents('.holder-slider__info');
+        let summ = $('.summ', parents).data('nights') * $('.nights', parents).val() + $('.summ', parents).data('guests') * $('.guests', parents).val();
+        $('.summ', parents).html('$' + summ);
+        });
+
+    $('.surfboard-box__circle').on('click', function(){
+      $(this).toggleClass('active')
+    });
 });
