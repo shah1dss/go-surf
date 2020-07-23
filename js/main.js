ca988c76abb7e2d206f3734733c673e9fc07a97a
shah1dss <ckeiter@bk.ru>
2020-07-24 00:54:15 +0300

    Сделана адаптация для блока с картой

--- js/main.js
+++ js/main.js
@@ -11,6 +11,12 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
+        responsive: [
+          {
+            breakpoint: 961,
+            settings: "unslick"
+          },
+        ]
     });
 
     $('.surf-slider').slick({
@@ -19,13 +25,63 @@ $(function(){
         slidesToShow: 4,
         slidesToScroll: 1,
         asNavFor: '.slider-map',
+        responsive: [
+          {
+            breakpoint: 1210,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 900,
+            settings: {
+              slidesToShow: 2,
+            }
+          },
+          {
+            breakpoint: 720,
+            settings: {
+              slidesToShow: 1,
+              centerMode: true
+            }
+          },
+          {
+            breakpoint: 426,
+            settings: {
+              slidesToShow: 1,
+              centerMode: false
+            }
+          },
+        ]
     });
     $('.slider-map').slick({
         slidesToShow: 8,
         slidesToScroll: 1,
         arrows: false,
         asNavFor: '.surf-slider',
-        focusOnSelect: true
+        focusOnSelect: true,
+        responsive: [
+          {
+            breakpoint: 1102,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 900,
+            settings: {
+              slidesToShow: 2,
+              centerMode: true
+            }
+          },
+          {
+            breakpoint: 720,
+            settings: {
+              slidesToShow: 1,
+              centerMode: true
+            }
+          },
+        ]
 
     });
     $('.holder__slider').slick({
